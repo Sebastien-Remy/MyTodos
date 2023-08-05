ee7c170f9052500e4cb821015d664b7294248b4f
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2023-08-05 15:48:16 +0200

    add todoitem

--- MyTodos/ToDoItem.swift
+++ MyTodos/ToDoItem.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class ToDoItem {
+    var title: String
+    var timestamp: Date
+    var isCirtical: Bool
+    var isCompleted: Bool
+    
+    init(title: String = "",
+         timestamp: Date = .now,
+         isCirtical: Bool = false,
+         isCompleted: Bool = false) {
+        self.title = title
+        self.timestamp = timestamp
+        self.isCirtical = isCirtical
+        self.isCompleted = isCompleted
+    }
+}
