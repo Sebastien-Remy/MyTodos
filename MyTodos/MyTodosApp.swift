48d14af7a652bbd581920abe3064e8fa2faae2ba
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2023-08-05 15:42:05 +0200

    Initial Commit

---
+++ MyTodos/MyTodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodosApp.swift
+//  MyTodos
+//
+//  Created by Sebastien REMY on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyTodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
