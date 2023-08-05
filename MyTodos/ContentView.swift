425c3d63b662288c4fc78efc01adc78765663ff8
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2023-08-05 16:22:44 +0200

    create ui

--- MyTodos/ContentView.swift
+++ MyTodos/ContentView.swift
@@ -6,19 +6,72 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    @State private var showCreate = false
+    @Query private var items: [ToDoItem]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            
+                            if item.isCritical {
+                                Image(systemName: "exclamationmark.3")
+                                    .symbolVariant(.fill)
+                                    .foregroundColor(.red)
+                                    .font(.largeTitle)
+                                    .bold()
+                            }
+                            
+                            Text(item.title)
+                                .font(.largeTitle)
+                                .bold()
+                            
+                            Text("\(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .shortened))")
+                                .font(.callout)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            withAnimation {
+                                item.isCompleted.toggle()
+                            }
+                        } label: {
+                            
+                            Image(systemName: "checkmark")
+                                .symbolVariant(.circle.fill)
+                                .foregroundStyle(item.isCompleted ? .green : .gray)
+                                .font(.largeTitle)
+                        }
+                    }
+                }
+                
+            }
+            
+            .toolbar {
+                ToolbarItem {
+                    Button(action: { showCreate.toggle()},
+                           label: { Label("Add Item",
+                                          systemImage: "plus")})
+                }
+            }
+            .sheet(isPresented: $showCreate, content: {
+                NavigationStack {
+                    CreateView()
+                }
+                .presentationDetents([.medium])
+            })
         }
-        .padding()
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    
+    #Preview {
+        ContentView()
+    }
--- MyTodos/ToDoItem.swift
+++ MyTodos/ToDoItem.swift
@@ -6,20 +6,22 @@
 //
 
 import Foundation
+import SwiftData
 
+@Model
 final class ToDoItem {
     var title: String
     var timestamp: Date
-    var isCirtical: Bool
+    var isCritical: Bool
     var isCompleted: Bool
     
     init(title: String = "",
          timestamp: Date = .now,
-         isCirtical: Bool = false,
+         isCritical: Bool = false,
          isCompleted: Bool = false) {
         self.title = title
         self.timestamp = timestamp
-        self.isCirtical = isCirtical
+        self.isCritical = isCritical
         self.isCompleted = isCompleted
     }
 }
